94fc64e63484d31a9170ed54f5a1555b5d4631be
Hayato Tsujii <97589878+CaptianHa@users.noreply.github.com>
2023-01-19 17:35:55 -0800

    Update keys.py

--- keys.py
+++ keys.py
@@ -1,6 +1,5 @@
-account_sid = 'AC620e2cf5f825a52e534c458daa013efd'
-auth_tocken = '505951863162f6c3c13bb053a853066a'
+account_sid = #twilio
+auth_tocken = #twilio
 
-twilio_number ='+19569839404'
-target_number = '+18587378507'
-# target_number = '+16714829003'
+twilio_number = #twilio number
+target_number = #target number
